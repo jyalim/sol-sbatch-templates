049aacde1de56d862b22f93157bbd8df83fa4ef8
Jason Yalim <yalim@asu.edu>
2022-11-27 22:33:36 -0700

    job name in verbose log

--- templates/template-rclone-job/rclone-sbatch.sh
+++ templates/template-rclone-job/rclone-sbatch.sh
@@ -6,18 +6,22 @@
 #SBATCH --mail-user=%u@asu.edu
 #SBATCH --export=NONE
 
+# ======================================================================
 # Remotes with paths are passed as positional arguments (ex. below):
 #   
 #   sbatch rclone-sbatch.sh drive:data/ dropbox:team_dir/data/
 #                           ^--1st arg  ^--2nd arg
-#
+# ======================================================================
+ 
 in="$1"
 to="$2"
 
 now=$(date +%F_%s)
 
+jobname="${SLURM_JOB_NAME// /_}"
+jobid="${SLURM_JOB_ID}"
 logdir="${HOME}/.local/var/rclone/transfers"
-logrec="${logdir}/${now}--rclone-${SLURM_JOB_ID}.log"
+logrec="${logdir}/${now}--rclone-${jobname}-${jobid}.log"
 
 ! [[ -d "$logdir" ]] && mkdir -pv "$logdir" || :
 
