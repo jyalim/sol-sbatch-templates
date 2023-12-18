35fdca7fb5a2748ac965da25ca1ea78dd35fab19
Jason Yalim <yalim@asu.edu>
2023-12-18 11:37:19 -0700

    init

--- templates/example-python-job/main_job.sh
+++ templates/example-python-job/main_job.sh
@@ -7,16 +7,13 @@
 #SBATCH --output=slurm.%j.out  ## job /dev/stdout record (%j expands -> jobid)
 #SBATCH --error=slurm.%j.err   ## job /dev/stderr record 
 #SBATCH --export=NONE          ## keep environment clean
-#SBATCH --mail-type=ALL        ## notify for any job state change
-#SBATCH --mail-user=%u@asu.edu ## notify email (%u expands -> username)
+#SBATCH --mail-type=ALL        ## notify <asurite>@asu.edu for any job state change
 
 echo "WHERE I AM FROM: $SLURM_SUBMIT_DIR"
 echo "WHERE AM I NOW: $(pwd)"
 echo "Hello World" > output_file
 echo "Created output file with 'Hello World'"
 
-echo "Purging modules"
-module purge
 echo "Loading python 3 from anaconda module"
 module load mamba/latest
 echo "Loading scientific computing python environment, scicomp"
--- templates/example-sequential-job/main_job.sh
+++ templates/example-sequential-job/main_job.sh
@@ -6,8 +6,7 @@
 #SBATCH --output=slurm.%j.out  ## job /dev/stdout record (%j expands -> jobid)
 #SBATCH --error=slurm.%j.err   ## job /dev/stderr record 
 #SBATCH --export=NONE          ## keep environment clean
-#SBATCH --mail-type=ALL        ## notify for any job state change
-#SBATCH --mail-user=%u@asu.edu ## notify email (%u expands -> username)
+#SBATCH --mail-type=ALL        ## notify <asurite>@asu.edu for any job state change
 
 echo "Hello World" > output_file
 
